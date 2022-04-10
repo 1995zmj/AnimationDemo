4507223598a9967a56551ec7ae952fc46f320728
1995zmj <784178252@qq.com>
2022-04-10 19:02:15 +0800

    初始化

---
+++ Source/AnimationDemo/AnimationDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnimationDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMATIONDEMO_API AAnimationDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
